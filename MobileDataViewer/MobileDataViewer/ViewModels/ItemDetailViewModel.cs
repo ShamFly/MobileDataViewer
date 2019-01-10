f2dfb6a56896bf9c693a7622e72588db99aa9c6f
Шамиль Мухаметдинов <shamfly@gmail.com>
2019-01-10 12:41:33 +0300

    Initial

---
+++ MobileDataViewer/MobileDataViewer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobileDataViewer.Models;
+
+namespace MobileDataViewer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
