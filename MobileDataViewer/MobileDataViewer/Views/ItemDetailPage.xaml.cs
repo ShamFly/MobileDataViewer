b9ff8b04e27e362f59b52c155814c11871fdb3b4
Шамиль Мухаметдинов <shamfly@gmail.com>
2019-01-31 15:55:25 +0300

    First data displayed

--- MobileDataViewer/MobileDataViewer/Views/ItemDetailPage.xaml.cs
+++ MobileDataViewer/MobileDataViewer/Views/ItemDetailPage.xaml.cs
@@ -1,30 +1,102 @@
-﻿using System;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
+﻿
+using Microcharts;
 using MobileDataViewer.Models;
 using MobileDataViewer.ViewModels;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Entry = Microcharts.Entry;
 
 namespace MobileDataViewer.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemDetailPage : ContentPage
     {
-        ItemDetailViewModel viewModel;
+        private List<Entry> entries = new List<Entry>
+        {
+            new Entry(200)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Январь",
+                ValueLabel="200"
+            },
+            new Entry(400)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Февраль",
+                ValueLabel="400"
+            },
+            new Entry(200)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Январь",
+                ValueLabel="200"
+            },
+            new Entry(400)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Февраль",
+                ValueLabel="400"
+            },
+            new Entry(200)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Январь",
+                ValueLabel="200"
+            },
+            new Entry(400)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Февраль",
+                ValueLabel="400"
+            },
+            new Entry(200)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Январь",
+                ValueLabel="200"
+            },
+            new Entry(400)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Февраль",
+                ValueLabel="400"
+            },
+            new Entry(200)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Январь",
+                ValueLabel="200"
+            },
+            new Entry(400)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Февраль",
+                ValueLabel="400"
+            },
+            new Entry(-100)
+            {
+                Color = SkiaSharp.SKColor.Parse("#FF1493"),
+                Label = "Март",
+                ValueLabel="-100"
+            }
+        };
+        private ItemDetailViewModel viewModel;
 
         public ItemDetailPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
 
             BindingContext = this.viewModel = viewModel;
+
+            Chart1.Chart = new BarChart() { Entries = entries };
         }
 
         public ItemDetailPage()
         {
             InitializeComponent();
 
-            var item = new Item
+            Item item = new Item
             {
                 Text = "Item 1",
                 Description = "This is an item description."
